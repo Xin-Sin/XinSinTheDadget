4ae067a6a65704ab2530093505c14f947ff920de
xinsin <2890826955@qq.com>
2022-07-18 19:31:07 +0800

    refactor:refactor project

--- Ctrl-CV/src/top/xinsin/CVMain.java
+++
@@ -1,9 +0,0 @@
-package top.xinsin;
-
-import top.xinsin.ui.MainWindow;
-
-public class CVMain {
-    public static void main(String[] args) {
-        new MainWindow();
-    }
-}
--- EatFish/src/top/xinsin/ui/MainWindow.java
+++
@@ -1,6 +0,0 @@
-package top.xinsin.ui;
-
-import javafx.scene.layout.AnchorPane;
-
-public class MainWindow extends AnchorPane {
-}
